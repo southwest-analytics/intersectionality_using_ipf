6fb8f8a47467684481085e99850c1230a6542660
Richard Blackwell <73821575+blackwellrd@users.noreply.github.com>
2026-09-06 11:51:30 +0100

    Pre-pub commit

--- process_census_files.R
+++ process_census_files.R
@@ -98,13 +98,24 @@
 # ════════════════════════════════════════════
 library(tidyverse)
 
+fnProcessMarginal <- function(filename, var){
+  df <- read.csv(filename)
+  
+  names(df) <- c(paste0(c(rep("AREA", 2), rep(var, 2)), rep(c("_CODE", "_DESC"), 2)), "OBS")
+  
+  df <- df %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% arrange(AREA_CODE) %>% mutate(across(.cols = c(1:2, 4), .fns = as.factor))
+
+  df[,paste0(var, "_DESC")] <- forcats::fct_reorder(df[,paste0(var, "_DESC")], df[,paste0(var, "_CODE")])
+
+  return(df)
+}
 
 fnProcessConstraint <- function(filename, vars){
   df <- read.csv(filename)
   
   names(df) <- c(paste0(c(rep("AREA", 2), rep(vars[1], 2), rep(vars[2], 2)), rep(c("_CODE", "_DESC"), 3)), "OBS")
   
-  df <- df %>% arrange(AREA_CODE) %>% mutate(across(.cols = c(1:2, 4, 6), .fns = as.factor))
+  df <- df %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% arrange(AREA_CODE) %>% mutate(across(.cols = c(1:2, 4, 6), .fns = as.factor))
   
   df[,paste0(vars[1], "_DESC")] <- forcats::fct_reorder(df[,paste0(vars[1], "_DESC")], df[,paste0(vars[1], "_CODE")])
   df[,paste0(vars[2], "_DESC")] <- forcats::fct_reorder(df[,paste0(vars[2], "_DESC")], df[,paste0(vars[2], "_CODE")])
@@ -115,30 +126,64 @@ fnProcessConstraint <- function(filename, vars){
 # 1. Load Data ----
 # ════════════════════════════════════════════
 
-# • 1.0. Low Level to High Level Geography Lookups ----
-# ─────────────────────────────────────────────────────
+# • 1.0. Low Level to High Level Geography Lookups and Units Translations ----
+# ────────────────────────────────────────────────────────────────────────────
+
+df_area_lu <- read.csv("data/OA21_LSOA21_MSOA21_LAD22_LU.csv") %>% 
+  # English output areas only
+  filter(grepl("^E", OA21CD)) %>%
+  select(-c(LSOA21NMW, MSOA21NMW, LAD22NMW, ObjectId)) %>% 
+  left_join(read.csv("data/OA21_RGN22_LU.csv") %>% 
+              select(1:3) %>% 
+              rename_with(.fn = ~c("OA21CD", "RGN22CD", "RGN22NM")),
+            by = "OA21CD")
+
+# Load translation files
+df_car_avail_tx <- fnProcessMarginal(filename = "data/marginals/unit_tx/car_availability_3_msoa.csv", var = "CAR_AVAIL")
+df_hhold_comp_tx <- fnProcessMarginal(filename = "data/marginals/unit_tx/hhold_comp_6_msoa.csv", var = "HHOLD_COMP")
+df_hhold_type_tx <- fnProcessMarginal(filename = "data/marginals/unit_tx/hhold_type_6_msoa.csv", var = "HHOLD_TYPE")
+df_tenure_tx <- fnProcessMarginal(filename = "data/marginals/unit_tx/tenure_5_msoa.csv", var = "TENURE")
 
 # • 1.1. Low Level Geography Marginals ----
 # ─────────────────────────────────────────
 
 # • • 1.1.1. Age ----
+df_age <- fnProcessMarginal(filename = "data/marginals/age_6_oa.csv", var = "AGE")
 # • • 1.1.2. Car availability ----
+df_car_avail <- fnProcessMarginal(filename = "data/marginals/hhold_car_availability_3_oa.csv", var = "CAR_AVAIL")
 # • • 1.1.3. Disability ----
+df_disability <- fnProcessMarginal(filename = "data/marginals/disability_3_oa.csv", var = "DISABILITY")
 # • • 1.1.4. Economic activity ----
+df_econ_act <- fnProcessMarginal(filename = "data/marginals/economic_activity_10_oa.csv", var = "ECON_ACT")
 # • • 1.1.5. Ethnicity ----
+df_ethnicity <- fnProcessMarginal(filename = "data/marginals/ethnicity_6_oa.csv", var = "ETHNICITY")
 # • • 1.1.6. General health ----
+df_health <- fnProcessMarginal(filename = "data/marginals/health_4_oa.csv", var = "HEALTH")
 # • • 1.1.7. Household composition ----
+df_hhold_comp <- fnProcessMarginal(filename = "data/marginals/hhold_comp_6_oa.csv", var = "HHOLD_COMP")
 # • • 1.1.8. Household type ----
+df_hhold_type <- fnProcessMarginal(filename = "data/marginals/hhold_type_6_oa.csv", var = "HHOLD_TYPE")
 # • • 1.1.9. Length of residence ----
+df_resid_length <- fnProcessMarginal(filename = "data/marginals/residence_length_6_oa.csv", var = "RESID_LENGTH")
 # • • 1.1.10. Main language ----
+df_main_lang <- fnProcessMarginal(filename = "data/marginals/main_lang_11_oa.csv", var = "MAIN_LANG")
 # • • 1.1.11. NS-SeC ----
+df_nssec <- fnProcessMarginal(filename = "data/marginals/nssec_10_oa.csv", var = "NSSEC")
 # • • 1.1.12. Occupation ----
+df_occupation <- fnProcessMarginal(filename = "data/marginals/occupation_10_oa.csv", var = "OCCUPATION")
 # • • 1.1.13. Proficiency in English ----
+df_english_prof <- fnProcessMarginal(filename = "data/marginals/english_prof_4_oa.csv", var = "ENGLISH_PROF")
 # • • 1.1.14. Qualifications ----
+df_quals <- fnProcessMarginal(filename = "data/marginals/quals_7_oa.csv", var = "QUALS")
 # • • 1.1.15. Religion ----
+df_religion <- fnProcessMarginal(filename = "data/marginals/religion_10_oa.csv", var = "RELIGION")
 # • • 1.1.16. Sex ----
+df_sex <- fnProcessMarginal(filename = "data/marginals/sex_2_oa.csv", var = "SEX")
 # • • 1.1.17. Tenure ----
+df_tenure <- fnProcessMarginal(filename = "data/marginals/hhold_tenure_5_oa.csv", var = "TENURE")
 # • • 1.1.18. Unpaid care ----
+df_unpaid_care <- fnProcessMarginal(filename = "data/marginals/unpaid_care_5_oa.csv", var = "UNPAID_CARE")
+
 
 # • 1.2. High Level Geography Constraints by Research Inclusion Barrier Domain ----
 # ─────────────────────────────────────────────────────────────────────────────────
@@ -239,5 +284,150 @@ df_hhold_comp_hhold_type_msoa <- fnProcessConstraint(filename = "data/constraint
 df_hhold_comp_unpaid_care_msoa<- fnProcessConstraint(filename = "data/constraints/hhold_comp_6_unpaid_care_5_msoa.csv", vars = c("HHOLD_COMP", "UNPAID_CARE"))
 #    • household type x unpaid care (HHOLD)
 df_hhold_type_unpaid_care_msoa <- fnProcessConstraint(filename = "data/constraints/hhold_type_6_unpaid_care_5_msoa.csv", vars = c("HHOLD_TYPE", "UNPAID_CARE"))
+
+# 2. Process Data ----
+# ════════════════════
+
+# • 2.1. Convert household marginals to persons ----
+# ──────────────────────────────────────────────────
+
+# Calculate the persons per household data at MSOA level
+df_car_avail_tx <- df_car_avail_tx %>% 
+  left_join(
+    df_car_avail %>% 
+      left_join(df_area_lu, by = c("AREA_CODE" = "OA21CD")) %>% 
+      group_by(MSOA21CD, CAR_AVAIL_DESC) %>%
+      summarise(HHOLDS = sum(OBS), .groups = "keep") %>%
+      ungroup(),
+    by = c("AREA_CODE" = "MSOA21CD", "CAR_AVAIL_DESC")) %>%
+  mutate(PERSONS_PER_HHOLD = if_else(HHOLDS==0, 0, OBS / HHOLDS),
+         MSOA21CD = AREA_CODE) %>%
+  left_join(df_area_lu %>% select(OA21CD, MSOA21CD), by = "MSOA21CD", relationship = "many-to-many") %>%
+  select(OA21CD, MSOA21CD, CAR_AVAIL_DESC, PERSONS_PER_HHOLD)
+
+df_hhold_comp_tx <- df_hhold_comp_tx %>% 
+  left_join(
+    df_hhold_comp %>% 
+      left_join(df_area_lu, by = c("AREA_CODE" = "OA21CD")) %>% 
+      group_by(MSOA21CD, HHOLD_COMP_DESC) %>%
+      summarise(HHOLDS = sum(OBS), .groups = "keep") %>%
+      ungroup(),
+    by = c("AREA_CODE" = "MSOA21CD", "HHOLD_COMP_DESC")) %>%
+  mutate(PERSONS_PER_HHOLD = if_else(HHOLDS==0, 0, OBS / HHOLDS),
+         MSOA21CD = AREA_CODE) %>%
+  left_join(df_area_lu %>% select(OA21CD, MSOA21CD), by = "MSOA21CD", relationship = "many-to-many") %>%
+  select(OA21CD, MSOA21CD, HHOLD_COMP_DESC, PERSONS_PER_HHOLD)
+
+df_hhold_type_tx <- df_hhold_type_tx %>% 
+  left_join(
+    df_hhold_type %>% 
+      left_join(df_area_lu, by = c("AREA_CODE" = "OA21CD")) %>% 
+      group_by(MSOA21CD, HHOLD_TYPE_DESC) %>%
+      summarise(HHOLDS = sum(OBS), .groups = "keep") %>%
+      ungroup(),
+    by = c("AREA_CODE" = "MSOA21CD", "HHOLD_TYPE_DESC")) %>%
+  mutate(PERSONS_PER_HHOLD = if_else(HHOLDS==0, 0, OBS / HHOLDS),
+         MSOA21CD = AREA_CODE) %>%
+  left_join(df_area_lu %>% select(OA21CD, MSOA21CD), by = "MSOA21CD", relationship = "many-to-many") %>%
+  select(OA21CD, HHOLD_TYPE_DESC, PERSONS_PER_HHOLD)
+
+df_tenure_tx <- df_tenure_tx %>% 
+  left_join(
+    df_tenure %>% 
+      left_join(df_area_lu, by = c("AREA_CODE" = "OA21CD")) %>% 
+      group_by(MSOA21CD, TENURE_DESC) %>%
+      summarise(HHOLDS = sum(OBS), .groups = "keep") %>%
+      ungroup(),
+    by = c("AREA_CODE" = "MSOA21CD", "TENURE_DESC")) %>%
+  mutate(PERSONS_PER_HHOLD = if_else(HHOLDS==0, 0, OBS / HHOLDS),
+         MSOA21CD = AREA_CODE) %>%
+  left_join(df_area_lu %>% select(OA21CD, MSOA21CD), by = "MSOA21CD", relationship = "many-to-many") %>%
+  select(OA21CD, MSOA21CD, TENURE_DESC, PERSONS_PER_HHOLD)
+
+# Apply persons per household to OA level data
+df_car_avail <- df_car_avail %>% 
+  left_join(df_car_avail_tx, by = c("AREA_CODE" = "OA21CD", "CAR_AVAIL_DESC")) %>%
+  mutate(AREA_CODE, AREA_DESC, CAR_AVAIL_CODE, CAR_AVAIL_DESC, OBS = round(OBS*PERSONS_PER_HHOLD, 0), .keep = "none")
+
+df_hhold_comp <- df_hhold_comp %>% 
+  left_join(df_hhold_comp_tx, by = c("AREA_CODE" = "OA21CD", "HHOLD_COMP_DESC")) %>%
+  mutate(AREA_CODE, AREA_DESC, HHOLD_COMP_CODE, HHOLD_COMP_DESC, OBS = round(OBS*PERSONS_PER_HHOLD, 0), .keep = "none")
+
+df_hhold_type <- df_hhold_type %>% 
+  left_join(df_hhold_type_tx, by = c("AREA_CODE" = "OA21CD", "HHOLD_TYPE_DESC")) %>%
+  mutate(AREA_CODE, AREA_DESC, HHOLD_TYPE_CODE, HHOLD_TYPE_DESC, OBS = round(OBS*PERSONS_PER_HHOLD, 0), .keep = "none")
+
+df_tenure <- df_tenure %>% 
+  left_join(df_tenure_tx, by = c("AREA_CODE" = "OA21CD", "TENURE_DESC")) %>%
+  mutate(AREA_CODE, AREA_DESC, TENURE_CODE, TENURE_DESC, OBS = round(OBS*PERSONS_PER_HHOLD, 0), .keep = "none")
+
+# • 2.2. Convert constraints into MSOA level and deal with missing MSOAs ----
+# ───────────────────────────────────────────────────────────────────────────
+
+df_area_lu %>% dplyr::filter(grepl("^E", OA21CD)) %>% distinct(MSOA21CD) %>% NROW()
+df_area_lu %>% dplyr::filter(grepl("^E", OA21CD)) %>% distinct(RGN22CD) %>% NROW()
+
+df_age_main_lang_rgn %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE) %>% NROW()
+df_main_lang_english_prof_rgn %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE) %>% NROW()
+df_main_lang_quals_rgn %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE) %>% NROW()
+
+df_age_english_prof_msoa %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE) %>% NROW()
+df_age_quals_msoa %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE) %>% NROW()
+
+df_missing <- df_english_prof_quals_msoa %>% 
+  left_join(df_area_lu %>% distinct(MSOA21CD, LAD22CD), by = c("AREA_CODE" = "MSOA21CD")) %>% 
+  group_by(LAD22CD, ENGLISH_PROF_CODE, ENGLISH_PROF_DESC, QUALS_CODE, QUALS_DESC) %>%
+  summarise(OBS = sum(OBS), .groups = "keep") %>% 
+  ungroup() %>%
+  group_by(LAD22CD) %>%
+  mutate(PCT = OBS / sum(OBS)) %>%
+  ungroup() %>% 
+  select(-OBS) %>%
+  inner_join(
+    df_area_lu %>% distinct(MSOA21CD, LAD22CD) %>% dplyr::filter(grepl("^E", MSOA21CD)) %>%
+      anti_join(df_english_prof_quals_msoa %>% dplyr::filter(grepl("^E", AREA_CODE)) %>% distinct(AREA_CODE), by = c("MSOA21CD" = "AREA_CODE")),
+    by = "LAD22CD", relationship = "many-to-many") %>%
+  left_join(df_age %>% 
+              left_join(df_area_lu, by = c("AREA_CODE" = "OA21CD")) %>% 
+              group_by(MSOA21CD) %>%
+              summarise(OBS = sum(OBS)) %>% 
+              ungroup(),
+            by = "MSOA21CD")
+
+names(df_missing)
+
+df_english_prof_quals_msoa <- df_english_prof_quals_msoa %>% 
+  bind_rows()
+
+df_age_english_prof_msoa %>% dplyr::filter(AREA_CODE=="E02003169") %>% group_by(AREA_CODE) %>% summarise(OBS = sum(OBS))
+df_english_prof_quals_msoa %>% dplyr::filter(AREA_CODE=="E02003169")
+
+  
+
+# 2. Iterative Proportional Fitting ----
+# ══════════════════════════════════════
+
+# 2.1. Domain 1: Communication ----
+# ─────────────────────────────────
+
+# Constraints
+# age x main language
+df_age_main_lang_rgn <- fnProcessConstraint(filename = "data/constraints/age_6_main_lang_11_rgn.csv", vars = c("AGE", "MAIN_LANG"))
+#    • age x proficiency in English
+df_age_english_prof_msoa <- fnProcessConstraint(filename = "data/constraints/age_6_english_prof_4_msoa.csv", vars = c("AGE", "ENGLISH_PROF"))
+#    • age x qualifications
+df_age_quals_msoa <- fnProcessConstraint(filename = "data/constraints/age_6_qual_7_msoa.csv", vars = c("AGE", "QUALS"))
+#    • main language x proficiency in English
+df_main_lang_english_prof_rgn <- fnProcessConstraint(filename = "data/constraints/main_lang_11_english_prof_4_rgn.csv", vars = c("MAIN_LANG", "ENGLISH_PROF"))
+#    • main language x qualifications
+df_main_lang_quals_rgn <- fnProcessConstraint(filename = "data/constraints/main_lang_11_quals_7_rgn.csv", vars = c("MAIN_LANG", "QUALS"))
+#    • proficiency in English x qualifications
+df_english_prof_quals_msoa <- fnProcessConstraint(filename = "data/constraints/english_prof_4_quals_7_msoa.csv", vars = c("ENGLISH_PROF", "QUALS"))
+
+#  Proxy census variables
+#    • age
+#    • main language
+#    • proficiency in English
+#    • qualifications
 
 
